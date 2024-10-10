e129d61fa58712f79704dbb12ea2951467438385
PedroBarrera <pedrodbarrerafdez@gmail.com>
2024-10-10 16:15:13 +0100

    1 commit

---
+++ public/generate.php
@@ -0,0 +1,47 @@
+<?php
+require '../vendor/autoload.php';
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+
+$data = $_POST['data'];
+$label = $_POST['label'];
+$imgName = $_POST['imgname'];
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data($data)
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+    ->labelText($label)
+    ->labelFont(new NotoSans(20))
+    ->labelAlignment(LabelAlignment::Center)
+    ->validateResult(false)
+    ->build();
+
+    /* $result->saveToFile(__DIR__.'/qrcode.png');*/
+    $result->saveToFile(__DIR__.'/img/' . $imgName . '.png');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GR Creator</title>
+</head>
+<body>
+  <h1>Código QR Generado</h1>
+  <p>
+    <img src="<?= $result->getDataUri() ?>" alt="codigo QR generado">
+  </p>
+</body>
+</html>
---
+++ public/prueba.php
@@ -0,0 +1,41 @@
+<?php
+require '../vendor/autoload.php';
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data('Nada esto es facilito')
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+    ->labelText('This is the label')
+    ->labelFont(new NotoSans(20))
+    ->labelAlignment(LabelAlignment::Center)
+    ->validateResult(false)
+    ->build();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Prueba</h1>
+  <p>El texto:</p>
+  <p>
+    <img src="<?= $result->getDataUri() ?>" alt="codigo QR generado">
+  </p>
+</body>
+</html>
